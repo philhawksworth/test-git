ddf9643e0fb111e4efdefef651ed285c97921971
Phil Hawksworth <phil@netlify.com>
2020-02-04 12:54:11 +0000

    ignore runs a file script?

---
+++ tagged.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Checking if tagged..."
+exit 1
+
+# if git describe --tags --exact-match HEAD; then echo "Tagged. We will deploy" exit 1; else echo "Nothing to build" exit 0; fi
